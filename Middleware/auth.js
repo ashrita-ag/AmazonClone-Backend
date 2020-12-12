85917799d866e06e0e86aa8a0ac2bd39f829a5bc
Ashrita Agarwal <ashritaagarwal.che18@itbhu.ac.in>
2020-12-12 23:19:17 +0530

    Update Files

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 const auth = (req, res, next) => {
   try {
     const token = req.header("Authorization");
-    if (!token) return res.status(400).json({ msg: "Invalid Auth" });
+    if (!token) return res.json({ msg: "Invalid Auth" });
     jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-      if (err) return res.status(400).json({ msg: "Invalid Auth" });
+      if (err) return res.json({ msg: "Invalid Auth" });
 
       req.user = user;
       next();
