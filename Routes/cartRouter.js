f077db4d8f574abe66e155366c6d5d4d96d2b549
Ashrita Agarwal <ashritaagarwal.che18@itbhu.ac.in>
2020-12-16 11:42:05 +0530

    Cart Done

--- Routes/cartRouter.js
+++ Routes/cartRouter.js
@@ -18,4 +18,20 @@ router.patch("/cart/add", auth, (req, res) => {
   }
 });
 
+router.patch("/cart/delete", auth, (req, res) => {
+  try {
+    User.findOneAndUpdate(
+      { _id: req.user.id },
+      { $pull: req.body },
+      { new: true },
+      (err, foundUser) => {
+        if (err) return res.json(err);
+        else return res.json(foundUser.cart);
+      }
+    );
+  } catch (err) {
+    return res.json({ msg: err.message });
+  }
+});
+
 module.exports = router;
