f6bdd44f743979df4f9ed2fa82c4105db218a2cd
Ashrita Agarwal <ashritaagarwal.che18@itbhu.ac.in>
2021-01-06 17:01:26 +0530

    Update Files

--- Routes/cartRouter.js
+++ Routes/cartRouter.js
@@ -34,6 +34,45 @@ router.patch("/delete", auth, (req, res) => {
   }
 });
 
+// router.patch("/update_cart", auth, (req, res) => {
+//   try {
+//     User.findById(req.user.id, (err, found) => {
+//       if (err) return res.json(err);
+//       else {
+//         const { cart } = found;
+//         const productID = req.body.product;
+//         const index = cart.findIndex((cartItem) => cartItem._id === productID);
+//         if (index > -1) {
+//           // found.update()
+//           cart[index].count = cart[index].count + 1;
+
+//           res.json(cart[index]);
+
+//         }
+//         else res.json({ cart, productID });
+
+
+//         // const index = cart.findIndex(
+//         //   (cartItem) => cartItem._id === req.item._id
+//         // );
+//         // if (index < 0) {
+//         //   found.cart.push(req.item);
+//         //   found
+//         //     .save()
+//         //     .then(() => {
+//         //       res.json(found.cart);
+//         //     })
+//         //     .catch((e) => res.json(e));
+//         // } else {
+//         // }
+//         // res.json(cart);
+//       }
+//     });
+//   } catch (err) {
+//     return res.json({ msg: err.message });
+//   }
+// });
+
 router.patch("/update", auth, (req, res) => {
   try {
     User.findOneAndUpdate(
