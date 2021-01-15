7be5c46b54f0662a9b366efb327a77b33311b1d7
Ashrita Agarwal <ashritaagarwal.che18@itbhu.ac.in>
2021-01-15 19:01:28 +0530

    Fixed Bugs

--- Models/productModel.js
+++ Models/productModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// const data = require("../data");
+const data = require("../data");
 
 const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -11,8 +11,17 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model("Product", productSchema);
 // Product.deleteMany({},(err) => console.log(err));
-// Product.insertMany(data.productsHome, (err) => console.log(err));
-// Product.insertMany(data.productsNew, (err) => console.log(err));
-// Product.insertMany(data.productsAdd, (err) => console.log(err));
+// Product.insertMany(data.productsHome, (err) => {
+//   if (err) console.log(err);
+//   else console.log("Home inserted");
+// });
+// Product.insertMany(data.productsNew, (err) => {
+//   if (err) console.log(err);
+//   else console.log("NewProduct inserted");
+// });
+// Product.insertMany(data.productsAdd, (err) => {
+//   if (err) console.log(err);
+//   else console.log("Additional inserted");
+// });
 
 module.exports = Product;
--- data.js
+++ data.js
@@ -1,18 +1,16 @@
 module.exports = {
   productsNew: [
     {
-      id: "1212",
       imageUrl:
         "https://images-eu.ssl-images-amazon.com/images/I/41LyBR2PH+L._AC_US218_FMwebp_QL70_.jpg",
       title:
-        "(Renewed) AmazonBasics RJ45 Cat-6 Ethernet Patch/LAN Cable -5Feet (1.5Meters)",
+        "(Renewed) AmazonBasics Ethernet Patch/LAN Cable -5 Feet (1.5 Meters)",
       price: "227",
       rating: 5,
       category: 3,
     },
 
     {
-      id: "1213",
       imageUrl:
         "https://m.media-amazon.com/images/I/61fPCf6ELcL._AC_UY218_.jpg",
       title:
@@ -22,7 +20,6 @@ module.exports = {
       category: 3,
     },
     {
-      id: "1214",
       imageUrl:
         "https://m.media-amazon.com/images/I/71gMx7NADKL._AC_UY218_.jpg",
       title: "(Renewed) Honor Sport Bluetooth Earphones (Red)",
@@ -31,7 +28,6 @@ module.exports = {
       category: 3,
     },
     {
-      id: "1215",
       imageUrl:
         "https://m.media-amazon.com/images/I/51YwPMOcJwL._AC_UY218_.jpg",
       title: "(Renewed) OPPO R15 Pro (Cosmic Purple, 6GB RAM, 128GB Storage)",
@@ -43,49 +39,44 @@ module.exports = {
 
   productsHome: [
     {
-      id: "1234",
       imageUrl:
-        "https://images-na.ssl-images-amazon.com/images/I/51WeSMjooDL._UL1024_.jpg",
-      title: "Women's Skinny Fit Jeans",
-      price: "1200",
-      rating: 3,
-      category: 2,
-    },
-
-    {
-      id: "1235",
-      imageUrl:
-        "https://images-na.ssl-images-amazon.com/images/I/919uzBZtVIL._UL1500_.jpg",
-      title: "Chronograph Analog Watch - for Men",
-      price: "1389",
+        "https://images-na.ssl-images-amazon.com/images/I/619EZzQYcDL._SL1500_.jpg",
+      title: "D-Link Dual Band Wireless Router",
+      price: "1399",
       rating: 4,
       category: 2,
     },
 
     {
-      id: "1236",
       imageUrl:
-        "https://images-na.ssl-images-amazon.com/images/I/81usHYQFH6L._UL1500_.jpg",
-      title: " Men's Vulcansied Canvas Sneakers",
-      price: "879",
-      rating: 4,
-      category: 2,
-    },
-
-    {
-      id: "1237",
-      imageUrl:
-        "https://images-na.ssl-images-amazon.com/images/I/51Hrp3SYtkL._UL1000_.jpg",
-      title: "Set of 4 Multilayer Bracelet for Women (Golden)",
-      price: "385",
+        "https://images-na.ssl-images-amazon.com/images/I/71nplbAMwzL._SL1202_.jpg",
+      title: "Samsung 23.5 inch Curved LED Backlit Computer Monitor",
+      price: "8699",
       rating: 5,
+      category: 2,
+    },
+
+    {
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/81tcTNLD0QL._SL1500_.jpg",
+      title: "4-Tier Metal Mesh Files Rack with slidable compartments",
+      price: "695",
+      rating: 4,
+      category: 2,
+    },
+
+    {
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/61XQBMrIn6L._SL1200_.jpg",
+      title: "Plastic Storage Basket, Multicolor",
+      price: "439",
+      rating: 3,
       category: 2,
     },
   ],
 
   productsAdd: [
     {
-      id: "1234",
       imageUrl:
         "https://images-na.ssl-images-amazon.com/images/I/51WeSMjooDL._UL1024_.jpg",
       title: "Women's Skinny Fit Jeans",
@@ -95,7 +86,6 @@ module.exports = {
     },
 
     {
-      id: "1235",
       imageUrl:
         "https://images-na.ssl-images-amazon.com/images/I/919uzBZtVIL._UL1500_.jpg",
       title: "Chronograph Analog Watch - for Men",
@@ -105,7 +95,6 @@ module.exports = {
     },
 
     {
-      id: "1236",
       imageUrl:
         "https://images-na.ssl-images-amazon.com/images/I/81usHYQFH6L._UL1500_.jpg",
       title: " Men's Vulcansied Canvas Sneakers",
@@ -115,7 +104,6 @@ module.exports = {
     },
 
     {
-      id: "1237",
       imageUrl:
         "https://images-na.ssl-images-amazon.com/images/I/51Hrp3SYtkL._UL1000_.jpg",
       title: "Set of 4 Multilayer Bracelet for Women (Golden)",
